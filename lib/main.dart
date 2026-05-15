cd6347a7c515e1be770c4f5b3fba33a2af99123a
behdad18 <h.kavoosi.ai@gmail.com>
2026-05-15 10:51:29 -0700

    add app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:app/theme.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,28 +12,44 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const deafaultTextStyle = TextStyle(
+      fontFamily: 'Yekan',
+      color: LightThemeColors.primarytextColor,
+    );
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: TextTheme(
+          bodyMedium: deafaultTextStyle,
+          headlineLarge: deafaultTextStyle.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+          bodySmall: deafaultTextStyle.copyWith(
+            color: LightThemeColors.secondarytextColor,
+          ),
+        ),
+        colorScheme: ColorScheme(
+          primary: LightThemeColors.primarycolor,
+          secondary: LightThemeColors.secondaryColor,
+          brightness: Brightness.light,
+          onPrimary: Colors.black,
+          onSecondary: Colors.white,
+          error: Colors.white,
+          onError: Colors.red,
+          surface: LightThemeColors.primarycolor,
+          onSurface: LightThemeColors.secondaryColor,
+        ),
       ),
-      home: ,
+      home: Directionality(textDirection: TextDirection.rtl, child: Home()),
     );
   }
 }
 
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class LightThemeColors{
+  static const primarycolor = Color(0xff217CF3);
+ static const secondaryColor = Color(0xff262A35);
+ static const primarytextColor = Color(0xff262A35);
+ static const secondarytextColor = Color(0xffB3B6BE);
+
+}
