df39df07bf469bf6736813268ae49a238f3ed8c4
behdad18 <h.kavoosi.ai@gmail.com>
2026-05-21 09:53:41 -0700

    refactore the code

--- lib/data/repository/product_repository.dart
+++ lib/data/repository/product_repository.dart
@@ -2,19 +2,20 @@ import 'package:app/common/http_client.dart';
 import 'package:app/data/data_source/product_data_source.dart';
 import 'package:app/data/product.dart';
 
-
 // Create a product repository instance with a remote data source
-final productRepository = ProductRepository(dataSource: ProductRemoteDataSource(httpClient: httpClient));
+final productRepository = ProductRepository(
+  dataSource: ProductRemoteDataSource(httpClient: httpClient),
+);
 
 // Abstract interface defining the contract for product repository
-abstract class IProductRepository{
+abstract class IProductRepository {
   // Get all products with specified sorting order
-  Future<List<ProductEntity>> getAll(int sort );
+  Future<List<ProductEntity>> getAll(int sort);
   // Search products by a search term
   Future<List<ProductEntity>> search(String searchTerm);
 }
 
-class ProductRepository implements IProductRepository{
+class ProductRepository implements IProductRepository {
   final IProductDataSource dataSource;
 
   ProductRepository({required this.dataSource});
@@ -22,5 +23,6 @@ class ProductRepository implements IProductRepository{
   Future<List<ProductEntity>> getAll(int sort) => dataSource.getAll(sort);
 
   @override
-  Future<List<ProductEntity>> search(String searchTerm) => dataSource.search(searchTerm);
+  Future<List<ProductEntity>> search(String searchTerm) =>
+      dataSource.search(searchTerm);
 }
