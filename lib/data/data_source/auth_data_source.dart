eb8706d7e1ede66d995bf0adb5e0891fa92301a2
behdad18 <h.kavoosi.ai@gmail.com>
2026-06-19 10:33:24 +0330

    implement login data_source for post  authentication

---
+++ lib/data/data_source/auth_data_source.dart
@@ -0,0 +1,50 @@
+import 'package:app/common/constants.dart';
+import 'package:app/common/http_client.dart';
+import 'package:app/common/http_response_validatore.dart';
+import 'package:app/data/auth_info.dart';
+import 'package:dio/dio.dart';
+
+abstract class IAuthDataSource {
+  Future<AuthInfo> login(String username, String passwpord);
+  Future<AuthInfo> register(String username, String passwpord);
+  Future<AuthInfo> refreshToken(String token);
+}
+
+class AuthRemoteDataSource
+    with HttpResponseValidatore
+    implements IAuthDataSource {
+  final Dio dio;
+
+  AuthRemoteDataSource({required this.dio});
+  @override
+  Future<AuthInfo> login(String username, String passwpord) async {
+    final response = await httpClient.post(
+      'auth/tokrn',
+      data: {
+        "grant_type": "password",
+        "client_id": 2,
+        "client_secret": Constants.clientSecret,
+        "username": username,
+        "password": passwpord,
+      },
+    );
+    validateRespone(response);
+
+    return AuthInfo(
+      accessToken: response.data['access_token'],
+      refreshToken: response.data['refresh_token'],
+    );
+  }
+
+  @override
+  Future<AuthInfo> refreshToken(String token) {
+    // TODO: implement refreshToken
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthInfo> register(String username, String passwpord) {
+    // TODO: implement register
+    throw UnimplementedError();
+  }
+}
