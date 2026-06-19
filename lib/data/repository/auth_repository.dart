057d5a5d7a35d15f0864ec0f235aab480d53c2cc
behdad18 <h.kavoosi.ai@gmail.com>
2026-06-19 10:32:38 +0330

    implement login repositori authentication

---
+++ lib/data/repository/auth_repository.dart
@@ -0,0 +1,23 @@
+import 'package:app/common/http_client.dart';
+import 'package:app/data/auth_info.dart';
+import 'package:app/data/data_source/auth_data_source.dart';
+import 'package:flutter/rendering.dart';
+
+final authRepository = AuthRepository(
+  dataSource: AuthRemoteDataSource(dio: httpClient),
+);
+
+abstract class IAuthRepository {
+  Future<void> login(String username, String passwpord);
+}
+
+class AuthRepository implements IAuthRepository {
+  final IAuthDataSource dataSource;
+
+  AuthRepository({required this.dataSource});
+  @override
+  Future<void> login(String username, String passwpord) async {
+    final AuthInfo authInfo = await dataSource.login(username, passwpord);
+    debugPrint("access token is :" + authInfo.accessToken);
+  }
+}
